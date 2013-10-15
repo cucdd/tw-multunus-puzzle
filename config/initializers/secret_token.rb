1584a845e3f454e15e42c3121464663055fe1aba
Rohit Kumar <rohitz1990@gmail.com>
2013-10-15 18:51:20 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwMultunusPuzzle::Application.config.secret_key_base = '1d3a21c1d250c7d602fc44710e2dbf2f351a61dae01841262ae0b7f214cf1f3c4be78dd56340bc81e7ad11de62b88cac5f957d1bd0a5879163a2964443e14b4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwMultunusPuzzle::Application.config.session_store :cookie_store, key: '_tw-multunus-puzzle_session'
