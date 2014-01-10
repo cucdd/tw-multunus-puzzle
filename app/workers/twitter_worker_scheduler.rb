6f59843d96bf8c0c166db76630a945dc5bd22a8c
Rohit Kumar` <rohitz1990@gmail.com>
2014-01-10 21:48:33 +0530

    change schedule time to 5 hours, run delay between each user to 20 minutes

--- app/workers/twitter_worker_scheduler.rb
+++ app/workers/twitter_worker_scheduler.rb
@@ -4,13 +4,14 @@ class TwitterWorkerScheduler
 
   sidekiq_options retry: 5
 
-  recurrence { hourly(4) }
+  recurrence { hourly(5) }
 
   def perform
     Person.redis='BUG' # bug in redic
     Person.all.each_with_index do |person, index|
       # Twitter API v1.1 has 15 minute rate limit window
-      TwitterWorker.perform_in(15.minutes * index, person.name)
+      # Each user will take 2-3 minutes to finish
+      TwitterWorker.perform_in(20.minutes * index, person.name)
     end
   end
 end
